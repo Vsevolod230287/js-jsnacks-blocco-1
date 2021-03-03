469446b76b7b1f880fbe958545cdce6085ebecbe
Vsevolod <cerneavschivsevolod@gmail.com>
2021-03-03 19:07:47 +0100

    js-snack-8-finito

---
+++ js-snack-8/main.js
@@ -0,0 +1,14 @@
+// Chiedi un numero di 4 cifre all’utente e calcola la somma
+// di tutte le cifre che compongono il numero.
+do {
+  var numero = prompt("Inserisci un numero di 4 cifre: ");
+} while (numero.length != 4);
+
+
+var sum = 0;
+
+for (i = 0; i < numero.length; i++) {
+   sum += parseInt(numero[i]);
+}
+
+console.log(sum);
