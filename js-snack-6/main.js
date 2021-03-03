82f556c48e7c2f290750fe29b9bfe5294aa60fd8
Vsevolod <cerneavschivsevolod@gmail.com>
2021-03-03 18:22:59 +0100

    js-snack-6 finito

---
+++ js-snack-6/main.js
@@ -0,0 +1,8 @@
+// Stampa il cubo dei primi N numeri, dove N è un numero
+// indicato dall’utente.
+
+var numero = prompt("Inserisci un numero:")
+
+for (i = 1; i <= numero; i++) {
+  console.log(i**3);
+}
